d33b6350b0b1a423c3cd30d4a8303933883aeb1c
Francisco Arenas <wook82@gmail.com>
2021-12-04 23:14:46 -0300

    agrego definiciopn de como usar los objetos ya definidos para funciones nuevas. Ademas de como definir operadores

---
+++ modules/src/main/java/com/platzi/functional/_06_reference_operator/NombresUtils.java
@@ -0,0 +1,25 @@
+package com.platzi.functional._06_reference_operator;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class NombresUtils {
+
+    static <T> List<T> getList(T ... elements) {
+        return Arrays.asList(elements);
+    }
+
+    public static void main(String[] args) {
+        List<String> profesores = getList("Nicolas", "Juan", "Zulama");
+
+        Consumer<String> printer = text -> System.out.println(text);
+        profesores.forEach(printer);
+        System.out.println("---");
+        profesores.forEach(System.out::println);
+
+        //podemos usar metodos definido en otros lados,
+        //podemos usar el operador de refencia,
+        // siempre y cuando utilice la misma cantidad de elementos y los mismos tipos de datos.
+    }
+}
